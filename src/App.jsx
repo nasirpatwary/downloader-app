d1b76dabafab65a8342cbabdf7ef4412515b8b8a
nasirhpatwary <nasirhpatwary@gmail.com>
2025-05-04 21:26:36 +0600

    icon added

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const SnaptubeDownloadPage = () => {
           <a
             download
             href="/Snaptube.apk"
-            className="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2"
+            className="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
           >
             Download 
           </a>
