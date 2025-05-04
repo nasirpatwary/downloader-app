bd26776a0d5be093e6b41d7d1ce99657bf0fc407
nasirhpatwary <nasirhpatwary@gmail.com>
2025-05-04 21:20:45 +0600

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Typewriter } from "react-simple-typewriter";
 import app from "./assets/app.jpg";
 import demu from "./assets/demu.jpg";
+import { FiDownload } from "react-icons/fi";
 const SnaptubeDownloadPage = () => {
   return (
     <div 
     className="space-y-8 bg-white p-4 font-sans text-center overflow-hidden">
       {/* Title & App Icon */}
       <div>
-        <a download href="/download.app">
+        <a download href="/Snaptube.apk">
           <img
             className="w-14 h-14 rounded-full mx-auto animate__animated animate__repeat-2 animate__animated animate__fadeInUp"
             src={app}
@@ -27,20 +28,21 @@ const SnaptubeDownloadPage = () => {
         </h2>
 
         {/* Download Button - Bottom Center */}
-        <div className="mt-6 animate__animated animate__fadeInUp">
+        <div className="mt-6 relative animate__animated animate__fadeInUp">
           <a
             download
-            href="/download.app"
-            className="text-black bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            href="/Snaptube.apk"
+            className="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2"
           >
-            Download
+            Download 
           </a>
+            <FiDownload className="absolute right-24 top-1 z-10" />
         </div>
       </div>
       <div>
       <img className="" src={demu} alt="" />
       </div>
-      <p className="text-xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"> Download app YouTube Instagram Facebook WhatsApp and Twitter </p>
+      <p className="text-xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"> Download app YouTube Instagram Facebook WhatsApp and Twitter Video Download</p>
       {/* Social Icons */}
       <div className="flex flex-wrap items-center text-black gap-4">
         <div className="flex items-center gap-5">
