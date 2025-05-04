2aa7eccfd99648d1591cd0d765748ed8bfcf836c
nasirhpatwary <nasirhpatwary@gmail.com>
2025-05-04 21:33:16 +0600

    done app

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -4,7 +4,8 @@ import demu from "./assets/demu.jpg";
 import { FiDownload } from "react-icons/fi";
 const SnaptubeDownloadPage = () => {
   return (
-    <div className="space-y-8 bg-white p-4 font-sans text-center overflow-hidden">
+    <div 
+    className="space-y-8 bg-white p-4 font-sans text-center overflow-hidden">
       {/* Title & App Icon */}
       <div>
         <a download href="/Snaptube.apk">
@@ -14,9 +15,8 @@ const SnaptubeDownloadPage = () => {
             alt=""
           />
         </a>
-        <h2 className="text-2xl leading-10 mt-4 bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-          Big opportunities <br />
-          <Typewriter
+        <h2 className="text-2xl leading-10 mt-4 bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">Big opportunities <br /> 
+        <Typewriter
             words={[" All Video Downloader."]}
             loop
             cursor
@@ -28,25 +28,21 @@ const SnaptubeDownloadPage = () => {
         </h2>
 
         {/* Download Button - Bottom Center */}
-        <div className="mt-6 relative animate__animated animate__fadeInUp">
+        <div className="mt-6 w-full relative animate__animated animate__fadeInUp">
           <a
             download
             href="/Snaptube.apk"
-            className="w-full block text-center bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            className="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
           >
-            Download
+            Download 
           </a>
-          <FiDownload className="absolute top-3 right-20 z-10" />
+            <FiDownload className="absolute right-24 top-1 z-10" />
         </div>
       </div>
       <div>
-        <img className="" src={demu} alt="" />
+      <img className="" src={demu} alt="" />
       </div>
-      <p className="text-xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-        {" "}
-        Download app YouTube Instagram Facebook WhatsApp and Twitter Video
-        Download
-      </p>
+      <p className="text-xl bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"> Download app YouTube Instagram Facebook WhatsApp and Twitter Video Download</p>
       {/* Social Icons */}
       <div className="flex flex-wrap items-center text-black gap-4">
         <div className="flex items-center gap-5">
